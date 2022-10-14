475baab1d153e01b4fd73da59412b376a8f95d2f
Azizbek <khasanjonovich@gmail.com>
2022-10-14 19:43:30 +0900

    GraphQL Yoga

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,18 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { createServer } from "@graphql-yoga/node";
 
-AppDataSource.initialize().then(async () => {
+const server = createServer({
+  schema: {
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "Hello from Yoga!",
+      },
+    },
+  },
+});
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
-
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
-
-}).catch(error => console.log(error))
+server.start();
